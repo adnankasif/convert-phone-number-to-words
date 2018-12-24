57709b67c53267f165e9439dffe3bb620c07ab04
adnankasif <adnankasif@gmail.com>
2018-12-25 02:02:50 +0530

    optimize code

--- lib/number_converter.rb
+++ lib/number_converter.rb
@@ -11,39 +11,9 @@ class NumberConverter
     end
 
     def get_input
-        puts "Enter 10 digit number (It should not contain 0 or 1):"
+        puts "Please enter a 10 digit number (It should not contain 0 or 1):"
         number = gets.chomp
-
-        check_valid_number(number)
-
-        key_characters = number.split("").map{|n| @keypad[n]}
-
-        begin
-            key_words = key_characters.shift.product(*key_characters).map(&:join)
-        rescue TypeError
-            return "The number you have entered is not a valid number. Please try again."
-        end
-
-        final_words = []
-        i = 2
-        while i < 7 do
-            a = key_words.map{|x| x[0..i]}.uniq
-
-            b = key_words.map{|y| y[i+1..-1]}.uniq
-
-            words_one = @words & a
-
-            words_two = @words & b
-
-            merge_words = words_one.product(words_two)
-            final_words << merge_words unless merge_words.empty?
-            i += 1
-        end
-
-        exact_matches = @words & key_words
-        final_words << exact_matches
-
-        p final_words.flatten(1)
+        get_key_combinations(number)
     end
 
     def get_keypad
@@ -66,8 +36,41 @@ class NumberConverter
         end
     end
 
-    
+    def get_key_combinations(number)
+        check_valid_number(number)
+        number_array = number.split("")
 
+        key_characters = number_array.map{|n| @keypad[n]}
+
+        begin
+            key_words = key_characters.shift.product(*key_characters).map(&:join)
+        rescue TypeError
+            return "The number you have entered is not a valid number. Please try again."
+        end
+
+        search_word_combinations(key_words)
+    end
+
+    def search_word_combinations(key_words)
+        final_words = []
+        i = 2
+        while i < 7 do
+            a = key_words.map{|x| x[0..i]}.uniq
+            b = key_words.map{|y| y[i+1..-1]}.uniq
+
+            words_one = @words & a
+            words_two = @words & b
+
+            merge_words = words_one.product(words_two)
+            final_words << merge_words unless merge_words.empty?
+            i += 1
+        end
+
+        exact_matches = @words & key_words
+        final_words << exact_matches
+
+        p final_words.flatten(1)
+    end
 end
 
 NumberConverter.new
